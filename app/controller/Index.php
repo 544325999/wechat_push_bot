c493cf9c7ed31f4622aaf497052ac3f6cf4d0b06
luoxiongteng <544325999@qq.com>
2022-08-25 17:47:00 +0800

    消息推送

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -2,9 +2,11 @@
 
 namespace app\controller;
 
-use App\Services\WechatTmpl;
+use App\Services\Wechat;
+use EasyWeChat\Factory;
 use support\Request;
-
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class Index
 {
@@ -12,14 +14,56 @@ class Index
 
     public function __construct()
     {
-        $this->service = new WechatTmpl();
+        $this->service = new Wechat();
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $this->service->index();
-        return json(['code' => 0, 'msg' => 'ok']);
+        $config = [
+            'app_id' => 'wxa7f1ef4c49583990',
+            'secret' => '199e5f5586d8b934e8204c6b5fd33333',
+            'token' => '172cdc8b87d5e765e7777a7f0f7de04c',
+            'response_type' => 'array',
+        ];
+        $app = Factory::officialAccount($config);
+        $symfony_request = new SymfonyRequest($request->get(), $request->post(), [], $request->cookie(), [], [], $request->rawBody());
+        $symfony_request->headers = new HeaderBag($request->header());
+        $app->rebind('request', $symfony_request);
 
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+        });
+
+        $response = $app->server->serve();
+        return $response->getContent();
     }
 
     public function view(Request $request)
