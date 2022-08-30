81e67d34504a1218f4d8bc74fe51b89b21e2b49d
luoxiongteng <544325999@qq.com>
2022-08-30 13:45:21 +0800

    message

--- app/Services/Account.php
+++ app/Services/Account.php
@@ -28,9 +28,9 @@ class Account
 
     public function __call($name, $arguments)
     {
+        // 根据类型去实现不同格式消息的处理方法
         $message = [
             'event' => '收到事件消息',
-            'text' => '收到文字消息',
             'image' => '收到图片消息',
             'voice' => '收到语音消息',
             'video' => '收到视频消息',
@@ -38,9 +38,7 @@ class Account
             'link' => '收到链接消息',
             'file' => '收到文件消息',
         ];
-        if (isset($arguments['MsgType'])) {
-            return $message[$arguments['MsgType']] ?? '收到其它消息';
-        }
+        return $message[$name] ?? '收到其它消息';
     }
 
 }
