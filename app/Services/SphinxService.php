3a3d98bed8d8a0b9a2d94762b570a2c73bfff748
544325999 <30817982+544325999@users.noreply.github.com>
2022-10-18 10:26:56 +0800

    sphinx

--- app/Services/SphinxService.php
+++ app/Services/SphinxService.php
@@ -16,6 +16,19 @@ class SphinxService
 
     public function run($msg) :array
     {
+        $client = new SphinxApi();
+        $q = $msg; //模拟关键字
+//$mode = SPH_MATCH_ALL;
+        $host = "127.0.0.1";// sphinx的服务地址  此处用的是本地服务 切记 不是数据库地址！！！
+        $port = 9900;// sphinx监听端口号
+        $index = "test1";   // 此处为配置文件中配置的索引项
+        $client->SetServer ( $host, $port );
+        $client->SetConnectTimeout(10);
+        $client->SetArrayResult(true);
+        $client->SetLimits(1,1000);//要获取所有数据是这里第三个参数控制，默认是1000,太大会影响效率
+//$cl->SetMatchMode(SPH_MATCH_ALL);//这个关闭它，不然会提示警告
+        $res = $client->Query( $q, $index );
+        return $res;
         return $this->filterResult(Jieba::cutForSearch($msg));
     }
 
