787b5cfcfc3735b70f1dde91d4f31562f4c41e42
544325999 <30817982+544325999@users.noreply.github.com>
2022-11-10 16:52:15 +0800

    Update XunSearchService.php

--- app/Services/XunSearchService.php
+++ app/Services/XunSearchService.php
@@ -13,9 +13,23 @@ class XunSearchService
     public function run($msg, $offset = 1, $limit = 5)
     {
         $search = $this->xs->search;
-        $search->setQuery($msg);
+        $search->setFuzzy()->setQuery($msg);
         $search->addWeight('log_Title', 'xunsearch');
         $search->setLimit($offset, $limit);
-        return $search->search();
+        return $this->handleResponses($search->search());
+    }
+
+    protected function handleResponses($data)
+    {
+        $result = [];
+        foreach ($data as $v) {
+           $result[] = [
+               'log_ID' => $v->log_ID,
+               'log_Title' => $v->log_Title,
+               'log_Intro' => $v->log_Intro,
+               'log_Content' => $v->log_Content,
+           ];
+        }
+       return $result;
     }
 }
